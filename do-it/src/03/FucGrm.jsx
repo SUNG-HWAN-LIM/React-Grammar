5619e536a7b66f7a634226e1d14a3d5ea858c5b9
SUNG-HWAN-LIM <sh414lim@gmail.com>
2021-03-06 21:20:44 +0900

    Fuc Component Grm

---
+++ do-it/src/03/FucGrm.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+function SFC(props, context) {
+    //클래스형 컴포넌트의 this.props값과 동일합니다
+    const { someContextValue } = context;
+    return <h1>Hello, {somePropValue}</h1>;
+}
+
+SFC.propTypes = { somePropValue: PropTypes.any };
+SFC.defaultProps = { somePropValue: 'default value' };
+
+export default SFC
