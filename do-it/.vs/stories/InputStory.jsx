bc2b072ed4e0ab8b29743b8055050f96b664bc22
SUNG-HWAN-LIM <sh414lim@gmail.com>
2021-03-08 20:45:47 +0900

    storybook

--- do-it/.vs/stories/InputStory.jsx
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Input from '../03/Input';
-
-storiesOf('input', module).add('기본 설정', () => <Input />);
